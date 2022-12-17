b67706cfe37f4e3b21003a62d85541c520896a9b
Alvaro11G <agarcia11746@gmail.com>
2022-12-17 10:59:54 -0700

    initial commit

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace APIsAndJSON
 {
     internal class OpenWeatherMapAPI
     {
+        public static void Weather()
+        {
+            var client = new HttpClient();
+
+            Console.WriteLine("Enter API key");
+            var apiKey = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter city name");
+            var cityName = Console.ReadLine();
+
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+            var response = client.GetStringAsync(weatherURL).Result;
+
+            var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+
+            Console.WriteLine(formattedResponse);
+        }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,30 @@ namespace APIsAndJSON
 {
     internal class RonVSKanyeAPI
     {
+        public static void KanyeQuote()
+        {
+            //connects to internet recieving and sending requests
+            var client = new HttpClient();
+
+            var kanyeURL = " https://api.kanye.rest";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye: \"{kanyeQuote}\"");
+        }
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"Ron: {ronQuote}");
+        }
+        
     }
 }
